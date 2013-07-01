2841cac3fb877107de564b0e6c867e404b4cca00
pamelabumpers <pamela.bumpers@aol.com>
2013-07-01 22:11:04 +0300

    init

---
+++ config.rb
@@ -0,0 +1,18 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/ari/"
+http_images_path = "/ari/images"
+http_fonts_path = "/ari/fonts"
+css_dir = "public/ari/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
